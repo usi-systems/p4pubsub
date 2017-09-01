f6cd69bb3945ed0754e8b71172e4e26a1df71441
Theo <theojepsen@gmail.com>
2017-09-01 11:59:12 -0700

    convert formula to DNF

---
+++ compiler/dnf.ml
@@ -0,0 +1,184 @@
+
+type var = string
+
+type formula =
+   | Empty
+   | Var of var
+   | Not of formula
+   | And of formula * formula
+   | Or of formula * formula
+
+let var_to_string = function
+   | Var(s) -> Printf.sprintf "%s" s
+   | _ -> raise (Failure "only takes vars")
+
+let rec formula_to_string t =
+   let rec and_to_string = function
+      (*
+      | And(p, q) -> Printf.sprintf "%s && %s" (and_to_string p) (and_to_string q)
+      *)
+      | And(p, q) -> Printf.sprintf "%s ∧ %s" (and_to_string p) (and_to_string q)
+      | p -> formula_to_string p
+   in
+   let rec or_to_string = function
+      (*
+      | Or(p, q) -> Printf.sprintf "%s || %s" (or_to_string p) (or_to_string q)
+      *)
+      | Or(p, q) -> Printf.sprintf "%s ∨ %s" (or_to_string p) (or_to_string q)
+      | p -> formula_to_string p
+   in
+   match t with 
+   | Empty -> ""
+   | Var(_) as v -> var_to_string v
+   | And(_, _) as p -> Printf.sprintf "(%s)" (and_to_string p)
+   | Or(_,_) as p-> Printf.sprintf "(%s)" (or_to_string p)
+   (*
+   | Not(a) -> Printf.sprintf "~%s" (formula_to_string a)
+   *)
+   | Not(a) -> Printf.sprintf "¬%s" (formula_to_string a)
+
+let print_form t = print_endline (formula_to_string t)
+
+let rec to_nnf = function
+   | Empty | Not(Empty) -> Empty
+   | Var(_) as p -> p
+   | Not(Var(_)) as p -> p
+   | Not(Not(p)) -> to_nnf p
+   | Not(And((Var(_) as p), (Var(_) as q))) -> Or(Not(p), Not(q))
+   | Not(Or((Var(_) as p), (Var(_) as q))) -> And(Not(p), Not(q))
+   | Not(And(p, q)) -> Or(to_nnf (Not(p)), to_nnf (Not(q)))
+   | Not(Or(p, q)) -> And(to_nnf (Not(p)), to_nnf (Not(q)))
+   | And(p, q) -> And(to_nnf p, to_nnf q)
+   | Or(p, q) -> Or(to_nnf p, to_nnf q)
+
+let test_to_nnf () =
+   let (a, b, c) = (Var("a"), Var("b"), Var("c")) in
+   let t1 = Not(Or(And(a, b), Or(a, b))) in
+   let t2 = Or(And(a, b), Or(a, b)) in
+   assert ((to_nnf a) = a);
+   assert ((to_nnf (Not(Not(a))) ) = a);
+   assert ((to_nnf (Not(a))) = Not(a));
+   assert ((to_nnf t1) = And(Or(Not(a), Not(b)), And(Not(a), Not(b))));
+   assert ((to_nnf t2) = Or(And(a, b), Or(a, b)))
+
+
+let rec conj_fold f acc conj = match conj with 
+   | And(((And(_,_) as c1)), ((And(_,_) as c2))) ->
+         conj_fold f (conj_fold f acc c1) c2
+   | And(((And(_,_) as c)), a)
+   | And(a, ((And(_,_) as c))) -> 
+         conj_fold f (f acc a) c
+   | And(a, b) -> 
+         f (f acc a) b
+   | a -> f acc a
+
+let conj_contains conj p =
+   conj_fold (fun acc q -> if acc then acc else p = q) false conj
+
+
+let conj_dedup conj =
+   let f acc p = match acc with
+      | Empty -> p
+      | c when conj_contains c p -> c
+      | c -> And(p, c)
+   in
+   conj_fold f Empty conj
+
+
+let is_conj_satisfiable conj =
+   let is_opposite p q = match (p, q) with
+      | (Not(Var(a)), Var(b)) when a = b -> true
+      | (Var(a), Not(Var(b))) when a = b -> true
+      | _ -> false
+   in
+   let contains_opposite acc p =
+      if acc then acc else
+         conj_fold (fun acc q -> if acc then acc else is_opposite p q) false conj
+   in
+   not (conj_fold contains_opposite false conj)
+
+
+let conj_is_subset conj_sup conj_sub =
+   conj_fold (fun acc p -> if acc then (conj_contains conj_sup p) else acc) true conj_sub
+
+let conj_eq conj1 conj2 =
+   (conj_is_subset conj1 conj2) && (conj_is_subset conj2 conj1)
+
+
+let rec disj_fold f acc disj = match disj with 
+   | Or(((Or(_,_) as d1)), ((Or(_,_) as d2))) ->
+         disj_fold f (disj_fold f acc d1) d2
+   | Or(((Or(_,_) as d)), a)
+   | Or(a, ((Or(_,_) as d))) -> 
+         disj_fold f (f acc a) d
+   | Or(a, b) -> 
+         f (f acc a) b
+   | a -> f acc a
+
+
+let disj_map f disj =
+   let mapper acc x = match acc with
+      | Empty -> f x
+      | d -> Or(d, f x)
+   in
+   disj_fold mapper Empty disj
+
+
+let disj_filter f disj =
+   let filterer acc x = match (acc, f x) with
+      | (Empty, true) -> x
+      | (Empty, false) -> Empty
+      | (y, true) -> Or(y, x)
+      | (y, false) -> y
+   in
+   disj_fold filterer Empty disj
+
+
+let disj_contains disj conj =
+   let f acc conj2 = match acc with
+      | true -> true
+      | false -> 
+            conj_eq conj conj2
+   in
+   disj_fold f false disj
+      
+
+let disj_dedup disj =
+   let f acc conj = match acc with
+      | Empty -> conj
+      | d when disj_contains d conj -> d
+      | d -> Or(d, conj)
+   in
+   disj_fold f Empty disj
+
+
+let to_dnf t =
+   let rec dist_left l r = match r with
+      | Or(p, q) -> Or(dist_left l p, dist_left l q)
+      | _ -> And(l, r)
+   in
+   let rec dist_right l r = match l with
+      | Or(p, q) -> Or(dist_right p r, dist_right q r)
+      | _ -> dist_left l r
+   in
+   let rec dist = function
+      | Or(p, q) -> Or(dist p, dist q)
+      | And(p, q) -> dist_right (dist p) (dist q)
+      | t -> t
+   in
+   let unreduced_dnf = dist (to_nnf t) in
+   disj_dedup (                                     (* remove duplicate conjunctions *)
+            disj_filter is_conj_satisfiable         (* remove unsatisfiable conjunctions *)
+              (disj_map conj_dedup unreduced_dnf))  (* remove duplicate preds *)
+
+
+let main () =
+   let (x, y, z) = (Var("x"), Var("y"), Var("z")) in
+   let t =  And(Or(x, Or(y, z)), And(Or(x, Or(Not(y), Not(z))), And(Or(y, Or(Not(x),
+   Not(z))), Or(z, Or(Not(x), Not(y)))))) in
+   print_form t;
+   print_form (to_dnf t);
+   print_endline ""
+;;
+
+main ()
