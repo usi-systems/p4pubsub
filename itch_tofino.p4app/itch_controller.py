e5f2a5e58804345a442a9889c83ce72608852562
Theo <theojepsen@gmail.com>
2017-09-14 11:55:57 -0700

    Subscribe to ITCH stock symbols with Tofino

---
+++ itch_tofino.p4app/itch_controller.py
@@ -0,0 +1,140 @@
+from appcontroller import AppController
+
+import threading
+import socket
+import os, os.path
+import time
+
+CONTROLLER_IPC_FILENAME = '/tmp/itch_controller.sock'
+
+def parseStocks(stocks_with_commas):
+    return ["%-8s" % st for st in stocks_with_commas.split(',')] # pad right with spaces
+
+
+class CustomAppController(AppController):
+
+    def __init__(self, *args, **kwargs):
+        AppController.__init__(self, *args, **kwargs)
+
+        self.topo, self.net = kwargs['topo'], kwargs['net']
+
+        self.ipc_thread = threading.Thread(target=self.ipc_thread)
+        self.ipc_thread.start()
+
+
+        self.stocks = {} # {stock: {mcgid: X, subscriptions: {host: mcnoderid}}}
+
+        self.host_ports = self.findHostPorts()
+
+        self.last_mcgid = 0
+        self.last_mcnoderid = -1
+
+    def findHostPorts(self):
+        mapping = {}
+        for h in self.net.hosts:
+            link = self.topo._host_links[h.name].values()[0]
+            mapping[link['host_ip']] = link['sw_port']
+        return mapping
+
+
+    def start(self):
+        AppController.start(self)
+
+        print "host_ports", self.host_ports
+
+    def stop(self):
+        AppController.stop(self)
+        self.sock.shutdown(socket.SHUT_RDWR)
+        self.sock.close()
+        self.ipc_thread.join()
+
+    def ipc_thread(self):
+        if os.path.exists(CONTROLLER_IPC_FILENAME):
+          os.remove(CONTROLLER_IPC_FILENAME)
+
+        self.sock = socket.socket( socket.AF_UNIX, socket.SOCK_DGRAM )
+        self.sock.bind(CONTROLLER_IPC_FILENAME)
+
+        print "Controller listening..."
+
+        while True:
+          data = self.sock.recv(2048)
+          if not data: break
+          self.handleReq(data)
+
+        self.sock.close()
+        os.remove(CONTROLLER_IPC_FILENAME)
+
+    def handleReq(self, data):
+        parts = [s.strip() for s in data.split('\t')]
+        if len(parts) < 1:
+            print "Malformed request:", data
+            return
+        cmd = parts[0]
+
+        if cmd == 'sub': # subscribe request
+            if len(parts) != 3:
+                print "Malformed sub request:", data
+                return
+
+            _, host, stocks = parts
+            self.subscribe(host, parseStocks(stocks))
+
+        elif cmd == 'unsub':
+            if len(parts) != 3:
+                print "Malformed sub request:", data
+                return
+
+            _, host, stocks = parts
+            self.unsubscribe(host, parseStocks(stocks))
+
+    def subscribe(self, host, stocks):
+        entries = []
+
+        mcast_groups = [] # [(mgid, ports)]
+
+        for stock in stocks:
+            if stock not in self.stocks:
+                self.last_mcgid += 1
+                self.stocks[stock] = dict(mcgid=self.last_mcgid, subscriptions=dict())
+                entries += ['mc_mgrp_create %d' % self.stocks[stock]['mcgid']]
+                stock_binary = ''.join(format(ord(x), '02x') for x in stock)
+                entries += ['table_add add_order set_mgid 0x%s => %d' % (stock_binary, self.stocks[stock]['mcgid']) ]
+
+            mcast_groups.append((self.stocks[stock]['mcgid'], [self.host_ports[host]]))
+
+        self.sendCommands(entries)
+        for g in mcast_groups: self.addMcastPorts(*g)
+
+    def unsubscribe(self, host, stocks):
+        for stock in stocks:
+            if stock not in self.subscriptions:
+                print "Cannot unsubscribe, because stock doesn't exist:", stock
+                return
+            if host not in self.subscriptions[stock]:
+                print "Cannot unsubscribe, because host is not subscribed:", stock, host
+                return
+            self.subscriptions[stock].remove(host)
+            # TODO: remove these MC groups and nodes from switch
+
+
+    def createMcastGroup(self, ports, mgid=None):
+        if mgid is None:
+            self.last_mgid += 1
+            mgid = self.last_mgid
+
+        commands = ['mc_mgrp_create %d' % mgid]
+        self.sendCommands(commands)
+
+        return mgid
+
+    def addMcastPorts(self, mgid, ports):
+        self.last_mcnoderid += 1
+        commands = ['mc_node_create %d %s' % (self.last_mcnoderid, ' '.join(map(str, ports)))]
+        results = self.sendCommands(commands)
+
+        handle = results[-1]['handle']
+        commands = ['mc_associate_node %d %d 1' % (mgid, results[-1]['handle'])]
+        #commands = ['mc_node_associate %d %d' % (mgid, results[-1]['handle'])]  # XXX BMV2
+        self.sendCommands(commands)
+
---
+++ itch_tofino.p4app/src/receiver.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+
+#include "libtrading/proto/nasdaq_itch50_message.h"
+#include "libtrading/proto/omx_moldudp_message.h"
+#include "../third-party/libtrading/lib/proto/nasdaq_itch50_message.c"
+
+#define BUFSIZE 2048
+
+char *my_hostname;
+
+void error(char *msg) {
+    perror(msg);
+    exit(0);
+}
+
+void send_to_controller(char *hostname, int port, char *msg, size_t len) {
+    struct sockaddr_in remoteaddr;
+
+    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("socket()");
+
+    struct hostent *server = gethostbyname(hostname);
+    if (server == NULL) {
+        fprintf(stderr, "bad hostname: %s\n", hostname);
+        exit(0);
+    }
+
+    bzero((char *) &remoteaddr, sizeof(remoteaddr));
+    remoteaddr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+            (char *)&remoteaddr.sin_addr.s_addr, server->h_length);
+    remoteaddr.sin_port = htons(port);
+
+    if (connect(sockfd, (struct sockaddr *)&remoteaddr, sizeof(remoteaddr)) < 0)
+        error("connect()");
+
+    int n = write(sockfd, msg, len);
+    if (n < 0)
+      error("write()");
+
+    close(sockfd);
+}
+
+void subscribe_to_stocks(char *controller_hostname, int port, char *stocks) {
+    char msg[2048];
+    int len = sprintf(msg, "sub\t%s\t%s\n", my_hostname, stocks);
+    send_to_controller(controller_hostname, port, msg, len);
+}
+
+int main(int argc, char *argv[]) {
+    int sockfd, i, n;
+    struct sockaddr_in localaddr;
+    struct sockaddr_in remoteaddr;
+    char buf[BUFSIZE];
+
+    char *stocks_with_commas = argv[1];
+    char *controller_hostname = argv[2];
+    int controller_port = atoi(argv[3]);
+    my_hostname = argv[4];
+    int port = atoi(argv[5]);
+
+    subscribe_to_stocks(controller_hostname, controller_port, stocks_with_commas);
+
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0)
+        error("socket()");
+
+    int optval = 1;
+    setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR,
+            (const void *)&optval , sizeof(int));
+
+    bzero((char *)&localaddr, sizeof(localaddr));
+    localaddr.sin_family = AF_INET;
+    localaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    localaddr.sin_port = htons(port);
+
+    if (bind(sockfd, (struct sockaddr *)&localaddr, sizeof(localaddr)) < 0)
+        error("bind()");
+
+    int remoteaddr_len = sizeof(remoteaddr);
+
+    while (1) {
+        n = recvfrom(sockfd, buf, BUFSIZE, 0,
+                (struct sockaddr *)&remoteaddr, &remoteaddr_len);
+        if (n < 0)
+            error("recvfrom()");
+
+        struct omx_moldudp_header *h = (struct omx_moldudp_header *)buf;
+        struct omx_moldudp_message *mm = (struct omx_moldudp_message *) (buf + sizeof(struct omx_moldudp_header));
+
+        struct itch50_message *m = (struct itch50_message *)(buf + sizeof(struct omx_moldudp_header) + sizeof(struct omx_moldudp_message));
+
+        int expected_size = itch50_message_size(m->MessageType);
+        if (expected_size != mm->MessageLength)
+            fprintf(stderr, "MessageType %c should have size %d, found %d\n", m->MessageType, expected_size, mm->MessageLength);
+
+        if (m->MessageType == ITCH50_MSG_ADD_ORDER) {
+            struct itch50_msg_add_order *ao = (struct itch50_msg_add_order *)m;
+            printf("Stock: '%s'\n", ao->Stock);
+        }
+
+        printf("Session: %d\nType: %c\n", h->Session[7], m->MessageType);
+        fflush(stdout);
+
+    }
+
+
+    return 0;
+}
