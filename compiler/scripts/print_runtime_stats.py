2d4edf40828a8dd26e433e12c4f3164d2dd802de
Theo <theojepsen@g-m-a-i-l-dotcom>
2017-09-19 19:49:09 -0700

    calculate number of states and unique tcam entries

--- compiler/scripts/print_runtime_stats.py
+++ compiler/scripts/print_runtime_stats.py
@@ -11,15 +11,19 @@ full_range_size = (int_size / 2) - 1
 tables = {}
 total_entries = 0
 total_miss_entries = 0
+states = set()
 
 with (sys.stdin if filename == '-' else open(filename, 'r')) as fd:
     for line in fd:
         if not line.startswith("table_add tbl_"): continue
 
+        state = int(line.split('=>')[1].split()[0])
+        full_tbl_name = line.split(' ', 2)[1]
+
         total_entries += 1
+        states.add(state)
         is_miss, is_range, is_exact = False, False, False
 
-        full_tbl_name = line.split(' ', 2)[1]
         if full_tbl_name.endswith('_miss'):
             is_miss = True
             tbl_name = full_tbl_name.rstrip('_miss')
@@ -29,9 +33,15 @@ with (sys.stdin if filename == '-' else open(filename, 'r')) as fd:
         elif full_tbl_name.endswith('_range'):
             is_range = True
             tbl_name = full_tbl_name[:-6]
+            range_match = line.split()[4]
 
         if tbl_name not in tables:
-            tables[tbl_name] = dict(exact=0, half_range=0, full_range=0, miss=0)
+            tables[tbl_name] = dict(exact=0,
+                    half_range=0,
+                    full_range=0,
+                    unique_half_ranges=set(),
+                    unique_full_ranges=set(),
+                    miss=0)
 
         if is_miss:
             total_miss_entries += 1
@@ -40,21 +50,28 @@ with (sys.stdin if filename == '-' else open(filename, 'r')) as fd:
             tables[tbl_name]['exact'] += 1
         elif '->' in line:
             tables[tbl_name]['full_range'] += 1
+            tables[tbl_name]['unique_full_ranges'].add(range_match)
         else:
             tables[tbl_name]['half_range'] += 1
+            tables[tbl_name]['unique_half_ranges'].add(range_match)
 
 for tbl_name in tables:
+    unique_half_ranges = len(tables[tbl_name]['unique_half_ranges'])
+    unique_full_ranges = len(tables[tbl_name]['unique_full_ranges'])
     print "[%s]" % tbl_name
-    print "\texact:\t\t", tables[tbl_name]['exact']
-    print "\thalf_range:\t", tables[tbl_name]['half_range']
-    print "\tfull_range:\t", tables[tbl_name]['full_range']
+    print "\texact:\t\t\t", tables[tbl_name]['exact']
+    print "\thalf_range:\t\t", tables[tbl_name]['half_range']
+    print "\tunique_half_range:\t", unique_half_ranges
+    print "\tfull_range:\t\t", tables[tbl_name]['full_range']
+    print "\tunique_full_range:\t", unique_full_ranges
     tcam_entries = half_range_size * tables[tbl_name]['half_range']
     tcam_entries += full_range_size * tables[tbl_name]['full_range']
-    print "\ttcam_entries:\t", tcam_entries
+    print "\ttcam_entries:\t\t", tcam_entries
+    print "\tunique_tcam_ranges:\t", half_range_size * unique_half_ranges + full_range_size * unique_full_ranges
     print
 
 
+print "states:\t\t\t", len(states)
 print "tables:\t\t\t", len(tables)
 print "total_miss_entries:\t", total_miss_entries
 print "total_entries:\t\t", total_entries
-
