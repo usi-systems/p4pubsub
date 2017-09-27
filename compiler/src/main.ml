5c6f62b76366d8af6c6041c534cbb3e7225b2e79
Theo <theojepsen@gmail.com>
2017-09-26 17:48:03 -0700

    Added args to compiler

--- compiler/src/main.ml
+++ compiler/src/main.ml
@@ -11,17 +11,6 @@ open Bdd_Table
 open P4_Runtime
 open P4_Program
 
-let create_and_print_bdd rules =
-   let formulas =
-      List.fold_left rules
-         ~init:[]
-         ~f:(fun l r -> (match r with Rule(Query(e), a) -> (to_dnf (formula_of_query e), a)::l))
-   in
-   let bdd = bdd_init 1000 in
-   List.iter formulas (fun x -> match x with (t, a) -> bdd_add_query bdd t a);
-   print_bdd bdd;
-   ()
-
 let save_to_file file s = Out_channel.write_all file ~data:s
 
 let print_position outx lexbuf =
@@ -42,30 +31,53 @@ let parse_with_error lexbuf =
 let string_of_rules rl =
    List.fold_left rl ~init:"" ~f:(fun s r -> s ^ (Format.asprintf "%a\n" format_rule r))
 
-let parse_and_print lexbuf =
-  let rules = parse_with_error lexbuf in
-  let tables = bdd_tables_create rules in
-  print_bdd_tables tables;
-  let runtime_conf = create_p4_runtime_conf tables in
-  print_endline ("/*\n" ^ (string_of_rules rules) ^ "*/\n");
+let output_prog tables runtime_conf out_dir =
+  if Sys.is_directory_exn out_dir then () else Unix.mkdir out_dir;
   (*
-  print_endline ("/*\n" ^ (dump_p4_runtime_conf runtime_conf) ^ "\n*/\n");
+  (match Sys.is_directory out_dir with
+  | No -> Unix.mkdir out_dir
+  | _ -> ());
   *)
-  save_to_file "generated_commands.txt" (dump_p4_runtime_commands runtime_conf);
-  save_to_file "generated_mcast_groups.txt" (dump_p4_runtime_mcast_groups runtime_conf);
-  save_to_file "p4src/generated_router.p4" (generate_p4_program (make_p4_fields tables.fields));
-  ()
+  save_to_file (Filename.concat out_dir "generated_commands.txt")
+          (dump_p4_runtime_commands runtime_conf);
+  save_to_file (Filename.concat out_dir "generated_mcast_groups.txt")
+          (dump_p4_runtime_mcast_groups runtime_conf);
+  save_to_file (Filename.concat out_dir "p4src/generated_router.p4")
+          (generate_p4_program (make_p4_fields tables.fields));
+          ()
 
-
-let loop filename () =
+let parse_rules_file filename =
   let inx = In_channel.create filename in
   let lexbuf = Lexing.from_channel inx in
   lexbuf.lex_curr_p <- { lexbuf.lex_curr_p with pos_fname = filename };
-  parse_and_print lexbuf;
-  In_channel.close inx
+  let rules = parse_with_error lexbuf in
+  In_channel.close inx;
+  rules
 
 let () =
-  Command.basic ~summary:"Compile Camus Query"
-    Command.Spec.(empty +> anon ("filename" %: file))
-    loop 
+  Command.basic ~summary:"Compile Camus Rules"
+    Command.Spec.(
+      empty
+      +> flag "-t" (optional string) ~doc:"filename Path to P4 template file"
+      +> flag "-o" (optional string) ~doc:"directory Output directory"
+      +> flag "-p" no_arg ~doc:" Print the parsed rules and exit"
+      +> flag "-v" (optional_with_default 1 int) ~doc:" Set verbosity level"
+      +> anon ("filename" %: file)
+    )
+    (fun template_filename opt_out_dir parse_and_exit verbosity filename () ->
+      let rules = parse_rules_file filename in
+      match parse_and_exit with
+      | true ->
+          print_endline (string_of_rules rules)
+      | false ->
+          let tables = bdd_tables_create rules in
+          if (verbosity > 0) then print_bdd_tables tables;
+          begin
+            match opt_out_dir with
+            | Some out_dir ->
+                let runtime_conf = create_p4_runtime_conf tables in
+                output_prog tables runtime_conf out_dir
+            | _ -> ()
+          end
+    )
   |> Command.run
