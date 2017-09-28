e5944fc2bf88a89a596b533435316d1c903b4848
Theo <theojepsen@gmail.com>
2017-09-28 08:51:33 -0700

    Generate and send multi-message mold packets

---
+++ itch_tools/src/generate_mold_messages.c
@@ -0,0 +1,216 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <libgen.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include "libtrading/proto/nasdaq_itch50_message.h"
+#include "libtrading/proto/omx_moldudp_message.h"
+#include "../third-party/libtrading/lib/proto/nasdaq_itch50_message.c"
+
+#include "common.c"
+
+char *progname;
+
+void error(char *msg) {
+    perror(msg);
+    exit(-1);
+}
+
+
+#define BUFSIZE 2048
+char buf[BUFSIZE];
+
+void usage(int rc) {
+    fprintf(rc == 0 ? stdout : stderr,
+    "Usage: %s [-m MIN_MSG_CNT] [-M MAX_MSG_CNT] [-p PKT_COUNT] [-r ITCH_FILE] [-o OUT_FILENAME]\n\
+\n\
+", progname);
+    exit(rc);
+}
+
+int random_int(int min, int max) {
+    if (min == max) return min;
+    return min + rand() % (max+1 - min);
+}
+
+char *stocks[] = {"ABC     ", "XYZ     ", "1234    "};
+
+int make_ao_msg(void *out_buf, int shares, int price) {
+    struct omx_moldudp_message *mm;
+    struct itch50_msg_add_order *ao;
+    uint16_t msg_len = sizeof(struct itch50_msg_add_order);
+
+    mm = (struct omx_moldudp_message *) out_buf;
+    mm->MessageLength = htons(msg_len);
+
+    ao = (struct itch50_msg_add_order *) ((void *)mm + 2);
+    bzero(ao, msg_len);
+    ao->MessageType = ITCH50_MSG_ADD_ORDER;
+    ao->BuySellIndicator = 'S';
+    ao->Price = htonl(price);
+    ao->Shares = htonl(shares);
+    memcpy(ao->Stock, stocks[0], 8);
+    return msg_len;
+}
+
+int read_next_msg(FILE *fh, void *out_buf) {
+
+    if (!fread(out_buf, sizeof(uint16_t), 1, fh)) {
+        if (feof(fh))
+            return 0;
+        else
+            error("fread()");
+    }
+
+    const uint16_t msg_len = ntohs(*(uint16_t *)(out_buf));
+    if (!fread(out_buf + sizeof(uint16_t), msg_len, 1, fh))
+        error("fread()");
+
+    return msg_len;
+}
+
+
+int main(int argc, char *argv[]) {
+    int opt, n;
+    int fd_out = 0;
+    FILE *fh_in = 0;
+    unsigned min_msgs = 0;
+    unsigned max_msgs = 0;
+    char *out_filename = 0;
+    char *in_filename = 0;
+    int pkt_count = 10;
+    char *extra_options = 0;
+    uint64_t tmp_seq_num;
+    struct omx_moldudp_header *h;
+    struct itch50_message *m;
+    int pkt_offset;
+    int msg_count;
+    int msg_num;
+    int msg_len;
+
+    progname = basename(argv[0]);
+
+    while ((opt = getopt(argc, argv, "ha:r:o:p:m:M:")) != -1) {
+        switch (opt) {
+            case 'a':
+                extra_options = optarg;
+                break;
+            case 'o':
+                out_filename = optarg;
+                break;
+            case 'r':
+                in_filename = optarg;
+                break;
+            case 'm':
+                min_msgs = atoi(optarg);
+                break;
+            case 'M':
+                max_msgs = atoi(optarg);
+                break;
+            case 'p':
+                pkt_count = atoi(optarg);
+                break;
+            case 'h':
+                usage(0);
+                break;
+            default: /* '?' */
+                usage(-1);
+        }
+    }
+
+
+    if (argc - optind != 0)
+        usage(-1);
+
+    if (max_msgs == 0)
+        max_msgs = min_msgs;
+
+    if (min_msgs == 0)
+        min_msgs = max_msgs;
+
+
+    if (extra_options) {
+    }
+
+    fd_out = 1;
+    if (out_filename) {
+        if (strcmp(out_filename, "-") != 0) {
+            fd_out = open(out_filename, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+            if (fd_out < 0)
+                error("open() out_filename");
+        }
+    }
+
+    if (in_filename) {
+        if (strcmp(in_filename, "-") == 0)
+            fh_in = stdin;
+        else {
+            fh_in = fopen(in_filename, "rb");
+            if (!fh_in)
+                error("fopen()");
+        }
+    }
+
+
+    int seq_num = 1;
+
+    short stop = 0;
+
+    while (!stop) {
+        msg_count = random_int(min_msgs, max_msgs);
+        h = (struct omx_moldudp_header *)buf;
+        bzero(h->Session, 8);
+        // TODO: use this when we use MoldUDP*64*
+        //tmp_seq_num = htonll(seq_num);
+        //memcpy(h->SequenceNumber, &tmp_seq_num, 8);
+        h->SequenceNumber = htonl(seq_num);
+
+        pkt_offset = sizeof(struct omx_moldudp_header);
+
+
+        for (msg_num = 0; msg_num < msg_count; msg_num++) {
+            if (fh_in) {
+                msg_len = read_next_msg(fh_in, buf + pkt_offset);
+                if (msg_len == 0) {
+                    stop = 1;
+                    break;
+                }
+            }
+            else {
+                msg_len = make_ao_msg(buf + pkt_offset, seq_num, msg_num);
+            }
+
+            pkt_offset += msg_len + 2;
+        }
+
+        h->MessageCount = htons(msg_num);
+
+        n = write(fd_out, buf, pkt_offset);
+        if (n < 0)
+            error("write()");
+
+        seq_num++;
+        if (seq_num > pkt_count && pkt_count != 0)
+            stop = 1;
+    }
+
+    if (fd_out > -1)
+        close(fd_out);
+
+    if (fh_in)
+        fclose(fh_in);
+
+    return 0;
+}
---
+++ itch_tools/src/send_mold_messages.c
@@ -0,0 +1,244 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <libgen.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include "libtrading/proto/nasdaq_itch50_message.h"
+#include "libtrading/proto/omx_moldudp_message.h"
+#include "../third-party/libtrading/lib/proto/nasdaq_itch50_message.c"
+
+#include "common.c"
+
+char *progname;
+int verbosity = 0;
+
+void error(char *msg) {
+    perror(msg);
+    exit(-1);
+}
+
+
+#define BUFSIZE 2048
+char buf[BUFSIZE];
+
+void usage(int rc) {
+    fprintf(rc == 0 ? stdout : stderr,
+    "Usage: %s [-a OPTIONS] [-v VERBOSITY] [-R MSG/S] [-r FILENAME] HOST PORT\n\
+\n\
+OPTIONS is a string of chars, which can include:\n\
+    a - print Add Order messages as TSV\n\
+\n\
+", progname);
+    exit(rc);
+}
+
+
+void timespec_diff(struct timespec *start, struct timespec *stop,
+                   struct timespec *result) {
+    if ((stop->tv_nsec - start->tv_nsec) < 0) {
+        result->tv_sec = stop->tv_sec - start->tv_sec - 1;
+        result->tv_nsec = stop->tv_nsec - start->tv_nsec + 1000000000;
+    } else {
+        result->tv_sec = stop->tv_sec - start->tv_sec;
+        result->tv_nsec = stop->tv_nsec - start->tv_nsec;
+    }
+}
+
+static void secs2ts(long double secs, struct timespec *ts) {
+    ts->tv_sec = secs;
+    ts->tv_nsec = (secs - ts->tv_sec) * 1.0e9;
+}
+
+#define MIN_SLEEP_NS 70000
+
+struct rate_limit_state {
+    struct timespec interval;
+    struct timespec last_call;
+    long outstanding_ns;
+};
+
+// XXX this does not work (it sleeps for too long)
+void rate_limiter(struct rate_limit_state *state) {
+    if (state->interval.tv_sec == 0 && state->interval.tv_nsec == 0) return;
+    struct timespec now;
+    struct timespec elapsed;
+    struct timespec wait;
+    if (state->last_call.tv_sec > 0) {
+        clock_gettime(CLOCK_REALTIME, &now);
+        timespec_diff(&state->last_call, &now, &elapsed);
+        timespec_diff(&elapsed, &state->interval, &wait);
+
+        if (wait.tv_sec > 0 || (wait.tv_sec >= 0 && wait.tv_nsec + state->outstanding_ns > MIN_SLEEP_NS)) {
+            wait.tv_nsec += state->outstanding_ns;
+            nanosleep(&wait, NULL);
+            state->outstanding_ns = 0;
+        }
+        else {
+            state->outstanding_ns += wait.tv_nsec;
+        }
+    }
+    else { // initialize state
+        state->outstanding_ns = 0;
+    }
+    clock_gettime(CLOCK_REALTIME, &state->last_call);
+}
+
+int main(int argc, char *argv[]) {
+    int opt;
+    int n, msg_num, msg_len, pkt_offset, pkt_size;
+    short msg_count;
+    struct stat s;
+    struct sockaddr_in remoteaddr;
+    char *filename = 0;
+    float msgs_per_s = 0;
+    struct rate_limit_state rate_state;
+    bzero(&rate_state, sizeof(rate_state));
+    uint64_t timestamp;
+    char *extra_options = 0;
+    int do_print_ao = 0;
+    struct omx_moldudp_header *h;
+    struct omx_moldudp_message *mm;
+    struct itch50_message *m;
+    struct itch50_msg_add_order *ao;
+
+    progname = basename(argv[0]);
+
+    while ((opt = getopt(argc, argv, "ha:m:v:r:R:")) != -1) {
+        switch (opt) {
+            case 'a':
+                extra_options = optarg;
+                break;
+            case 'r':
+                filename = optarg;
+                break;
+            case 'R':
+                msgs_per_s = atof(optarg);
+                break;
+            case 'v':
+                verbosity = atoi(optarg);
+                break;
+            case 'h':
+                usage(0);
+                break;
+            default: /* '?' */
+                usage(-1);
+        }
+    }
+
+    if (argc - optind != 2)
+        usage(-1);
+
+    if (extra_options) {
+        if (strchr(extra_options, 'a'))
+            do_print_ao = 1;
+    }
+
+    char *hostname = argv[optind];
+    int port = atoi(argv[optind+1]);
+
+    long double min_interval_secs =msgs_per_s > 0 ? 1 / msgs_per_s : 0;
+    secs2ts(min_interval_secs, &rate_state.interval);
+    //printf("min_interval: %lds %ldns\n", rate_state.interval.tv_sec, rate_state.interval.tv_nsec);
+
+    FILE *fh = stdin;
+    if (filename) {
+        if (strcmp(filename, "-") != 0) {
+            fh = fopen(filename, "rb");
+            if (!fh)
+                error("fopen()");
+        }
+    }
+
+
+    if (verbosity > 0)
+        fprintf(stderr, "Replaying from %s\n", filename ? filename : "STDIN");
+
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0)
+        error("socket()");
+
+    if (strcmp(hostname, "255.255.255.255") == 0) {
+        int enable_bcast = 1;
+        if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &enable_bcast, sizeof(int)) < 0)
+            error("setsockopt() SO_BROADCAST");
+    }
+
+
+    struct hostent *server = gethostbyname(hostname);
+    if (server == NULL) {
+        fprintf(stderr, "bad hostname: %s\n", hostname);
+        exit(0);
+    }
+
+    bzero((char *) &remoteaddr, sizeof(remoteaddr));
+    remoteaddr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr,
+            (char *)&remoteaddr.sin_addr.s_addr, server->h_length);
+    remoteaddr.sin_port = htons(port);
+
+    while (1) {
+        pkt_offset = sizeof(struct omx_moldudp_header);
+
+        if (!fread(buf, pkt_offset, 1, fh)) {
+            if (feof(fh))
+                break;
+            else
+                error("fread()");
+        }
+
+        h = (struct omx_moldudp_header *)buf;
+        msg_count = ntohs(h->MessageCount);
+
+        for (msg_num = 0; msg_num < msg_count; msg_num++) {
+            if (!fread(buf+pkt_offset, 2, 1, fh))
+                error("fread()");
+
+            mm = (struct omx_moldudp_message *) (buf + pkt_offset);
+            msg_len = ntohs(mm->MessageLength);
+
+            if (!fread(buf+pkt_offset+2, msg_len, 1, fh))
+                error("fread()");
+
+            m = (struct itch50_message *)(buf + pkt_offset + 2);
+            int expected_size = itch50_message_size(m->MessageType);
+            if (expected_size != msg_len)
+                fprintf(stderr, "MessageType %c should have size %d, found %d\n", m->MessageType, expected_size, msg_len);
+
+            if (m->MessageType == ITCH50_MSG_ADD_ORDER) {
+                ao = (struct itch50_msg_add_order *)m;
+                if (do_print_ao)
+                    print_add_order(ao);
+                timestamp = htonll(ns_since_midnight());
+                memcpy(ao->Timestamp, (void*)&timestamp + 2, 6);
+            }
+
+            pkt_offset += msg_len + 2;
+        }
+
+        if (sendto(sockfd, buf, pkt_offset, 0,
+                    (struct sockaddr *)&remoteaddr, sizeof(remoteaddr)) < 0)
+            error("sendto()");
+
+        if (verbosity > 1)
+            printf("Sent %d bytes\n", pkt_offset);
+
+    }
+
+
+    close(sockfd);
+    fclose(fh);
+
+    return 0;
+}
