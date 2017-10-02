6e7bc27922c224101ff447d4df317d003ad9162d
Theo <theojepsen@gmail.com>
2017-10-01 18:45:44 -0700

    plot multiple xy lines on one graph

--- plot_scripts/plot_xy.py
+++ plot_scripts/plot_xy.py
@@ -1,13 +1,35 @@
 #!/usr/bin/env python
 
+# Plot one or more files on the same X axis
+
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-filename = sys.argv[1]
+filenames = sys.argv[1:]
 
-data = np.loadtxt(filename, unpack=True)
+colors = ['g', 'r', 'b', 'c', 'o']
+color_idx = -1
+def nextColor():
+    global color_idx
+    color_idx = (color_idx + 1) % len(colors)
+    return colors[color_idx]
 
-plt.plot(data[0], data[1], marker=None)
+handles = []
+labels = []
+
+fig, ax = plt.subplots()
+
+for i,filename in enumerate(filenames):
+    data = np.loadtxt(filename, unpack=True)
+
+    h, = ax.plot(data[0], data[1], color=nextColor(), marker=None, label=filename)
+    handles.append(h)
+    labels.append(h.get_label())
+
+    if i+1 < len(filenames):
+        ax = ax.twinx()
+
+plt.legend(handles, labels, loc='upper right')
 
 plt.show()
