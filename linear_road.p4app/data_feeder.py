379fd34694310ee9807bf9d02e4c385491ac4b10
Theo <theojepsen@g-m-a-i-l-dotcom>
2017-11-10 13:39:21 -0800

    fixed data_feeder

--- linear_road.p4app/data_feeder.py
+++ linear_road.p4app/data_feeder.py
@@ -3,20 +3,24 @@ import sys
 import argparse
 from controller_rpc import RPCClient
 from datasource import LRDataSource
-from lr_proto import LRProducer, AccntBalReq, parseHostAndPort
+from linear_road import *
 from time import sleep
 
 parser = argparse.ArgumentParser(description='Send stream from a file')
 parser.add_argument('filename', help='Data source file', type=str)
 parser.add_argument('dst', help='host:port to send messages to', type=str)
+parser.add_argument('--controller', '-c', help='connect to the controller',
+                        action="store_true", default=False)
 args = parser.parse_args()
 
 toll_settings = dict(min_spd=40, min_cars=5, base_toll=1)
 
 dst_host, dst_port = parseHostAndPort(args.dst)
 producer = LRProducer(dst_host, dst_port)
-cont = RPCClient()
-cont.setToll(**toll_settings)
+
+if args.controller:
+    cont = RPCClient()
+    cont.setToll(**toll_settings)
 
 with LRDataSource(args.filename) as ds:
     for msg in ds:
