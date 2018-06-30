a568b7a929db63334f8bd276e27ba6b08988ec1f
Theo <theojepsen@g-m-a-i-l-dotcom>
2018-06-30 14:44:57 -0700

    unique terminals, better hashing

--- bdd/bdd.ml
+++ bdd/bdd.ml
@@ -5,30 +5,27 @@ open Core
  *)
 
 type uid = int
-type bdd_result = int
 
-let hash_str (s:string) : int =
-  let x = ref 0 in
-  String.iter s (fun c -> x := !x * 3571 + (Caml.Char.code c));
-  !x
+module StringMap = Map.Make(String)
 
-module Var = struct 
+module Var = struct
   type op = Lt | Gt | Eq
   type literal = string
   type t = literal * op * int
+  type assignments = int StringMap.t (* map literals to assigned values *)
   let compare (x: t) (y: t) =
     match x, y with
     | (a, _, _), (b, _, _) when a <> b -> Pervasives.compare a b
-    | (_, Lt, _), (_, Gt, _) 
-    | (_, Lt, _), (_, Eq, _) 
+    | (_, Lt, _), (_, Gt, _)
+    | (_, Lt, _), (_, Eq, _)
     | (_, Gt, _), (_, Eq, _) -> -1
-    | (_, Eq, _), (_, Gt, _) 
-    | (_, Eq, _), (_, Lt, _) 
+    | (_, Eq, _), (_, Gt, _)
+    | (_, Eq, _), (_, Lt, _)
     | (_, Gt, _), (_, Lt, _) -> 1
     | (_, _, j), (_, _, k) -> Pervasives.compare j k
 
   let equal (x: t) (y:t) = x = y
-  let fmt (x: t) =
+  let format_t (x: t) =
     match x with
     | (l, Lt, i) -> l ^ " < " ^ (string_of_int i)
     | (l, Gt, i) -> l ^ " > " ^ (string_of_int i)
@@ -46,7 +43,7 @@ module Var = struct
     | (_, Eq, j), (_, Lt, k)
     | (_, Lt, k), (_, Eq, j) -> j >= k
     | (_, Eq, j), (_, Eq, k) -> j <> k
-    
+
   let subset (sub: t) (sup: t) =
     match sub, sup with
     | (a, _, _), (b, _, _) when a <> b -> false
@@ -57,85 +54,80 @@ module Var = struct
     | (_, Eq, j), (_, Lt, k) -> k > j
     | _ -> false
 
-  let hash (x: t) =
+  let hash (x: t) = Hashtbl.hash x
+
+  let eval (x: t) (a: assignments) : bool =
     match x with
-    | a, Lt, i -> 1 + (hash_str a) + i
-    | a, Gt, i -> 2 + (hash_str a) + i
-    | a, Eq, i -> 3 + (hash_str a) + i
-    
+    | l, Lt, i -> (StringMap.find_exn a l) < i
+    | l, Gt, i -> (StringMap.find_exn a l) > i
+    | l, Eq, i -> (StringMap.find_exn a l) = i
+
 end
 
-module IntSet = Set.Make(Int)
+type bdd_label = int
 
 type bdd_node =
 | L of leaf
-| N of node                (* decision node *)
-and node = {uid: uid; var: Var.t; low: bdd_node; high: bdd_node }
-and leaf = {leaf_uid: uid; labels: bdd_result list}
+| N of decision_node
+and leaf = {leaf_uid: uid; labels: bdd_label list}
+and decision_node = {uid: uid; var: Var.t; low: bdd_node; high: bdd_node }
 
-let uid = function 
+let uid = function
   | L l -> l.leaf_uid
   | N n -> n.uid
 
-let node_equal n1 n2 =
-  Var.compare n1.var n2.var = 0
-  && uid n1.low = uid n2.low 
-  && uid n1.high = uid n2.high
-
-let leaf_equal l1 l2 =
-  l1.labels = l2.labels
+let node_equal x y =
+  match x, y with
+  | N n1, N n2 ->
+      Var.compare n1.var n2.var = 0
+      && uid n1.low = uid n2.low
+      && uid n1.high = uid n2.high
+  | L l1, L l2 ->
+      l1.labels = l2.labels
+  | N _, L _ | L _, N _ -> false
 
 
 module NodeH = struct
-  type t = node
+  type t = bdd_node
   let equal = node_equal
-  let hash n = 
-    let (+) x y = x * 3571 + y in 
-    (Var.hash n.var + uid n.low + uid n.high) land Int.max_value
-end
-
-module LeafH = struct
-  type t = leaf
-  let equal = leaf_equal
-  let hash l = 
-    let (+) x y = x * 3571 + y in 
-    (List.fold ~init:0 ~f:(+) l.labels) land Int.max_value
+  let hash node =
+    match node with
+    | N n ->
+        (Hashtbl.hash (Var.hash n.var, uid n.low, uid n.high)) land Int.max_value
+    | L l ->
+        (Hashtbl.hash l.labels) land Int.max_value
 end
 
 module NodeWeakHS = Caml.Weak.Make(NodeH)
-
-
 let table = NodeWeakHS.create 1337 (* weak hash set*)
 let next_uid = ref 2      (* global uid counter *)
 
-module LeafWeakHS = Caml.Weak.Make(LeafH)
-let leaf_table = LeafWeakHS.create 1337
 
 let mk_node var low high =
-  if uid low = uid high           
+  if uid low = uid high
   then low
-  else 
-    begin 
-      let n1 = {uid = !next_uid; var; low; high} in 
-      let n2 = NodeWeakHS.merge table n1 in 
-      if phys_equal n1 n2 
+  else
+    begin
+      let n1 = N {uid = !next_uid; var; low; high} in
+      let n2 = NodeWeakHS.merge table n1 in
+      if phys_equal n1 n2
       then incr next_uid;
-      N n2
+      n2
     end
 
 let mk_leaf lbls =
-  let l1 = {leaf_uid = !next_uid; labels = lbls} in 
-  let l2 = LeafWeakHS.merge leaf_table l1 in 
-  if phys_equal l1 l2 
+  let l1 = L {leaf_uid = !next_uid; labels = lbls} in
+  let l2 = NodeWeakHS.merge table l1 in
+  if phys_equal l1 l2
     then incr next_uid;
-  L l2
+  l2
 
 let empty_leaf = mk_leaf []
 
 let rec mergebdd x y =
   match x, y with
   | L {labels = lbls1}, L {labels = lbls2} ->                 (* both leaves *)
-      mk_leaf (lbls1 @ lbls2)
+      mk_leaf (List.dedup_and_sort (lbls1 @ lbls2))
   | (L _ as l), N {var = var; low = low; high = high; _}
   | N {var = var; low = low; high = high; _}, (L _ as l) ->   (* leaf and node *)
       mk_node var (mergebdd low l) (mergebdd high l)
@@ -181,7 +173,7 @@ let write_dot (bdd:bdd_node) : unit =
         Caml.Hashtbl.add visited u 0;
         match u with
         | N {uid = i; var = v; low = l; high = h;} ->
-            Printf.fprintf oc "n%d [label=\"%s\"];\n" i (Var.fmt v);
+            Printf.fprintf oc "n%d [label=\"%s\"];\n" i (Var.format_t v);
             Printf.fprintf oc "n%d -> n%d [style=\"dashed\"];\n" i (uid l);
             Printf.fprintf oc "n%d -> n%d;\n" i (uid h);
             w l; w h
@@ -194,14 +186,14 @@ let write_dot (bdd:bdd_node) : unit =
   Printf.fprintf oc "}";
   Out_channel.close oc
 
-type true_or_false_literals =
+type true_or_false_var =
   | T of Var.t
   | F of Var.t
 
-type conj =
-  true_or_false_literals list
+type conjunction =
+  true_or_false_var list
 
-let rec conj_to_bdd (formula:conj) lbl =
+let rec conj_to_bdd (formula:conjunction) lbl =
   match formula with
   | T q::[] -> mk_node q empty_leaf (mk_leaf [lbl])
   | F q::[] -> mk_node q (mk_leaf [lbl]) empty_leaf
@@ -209,8 +201,17 @@ let rec conj_to_bdd (formula:conj) lbl =
   | F q::t -> mk_node q (conj_to_bdd t lbl) empty_leaf
   | _ -> raise (Failure "unreachable")
 
+let rec eval_bdd (u:bdd_node) (a: Var.assignments) : bdd_label list =
+  match u with
+  | N {var = v; low = l; high = h} ->
+      if Var.eval v a then
+        eval_bdd h a
+      else
+        eval_bdd l a
+  | L {labels = lbls} -> lbls
 
-let () = 
+
+let () =
   let open Var in
   (*
   let a = conj_to_bdd [T("a", Eq, 1); T("b", Eq, 2); T("c", Eq, 3)] 1 in
