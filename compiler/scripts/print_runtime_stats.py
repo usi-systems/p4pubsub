2d87a58584d2531ed3f5af99db736a69a93339db
Theo <theojepsen@g-m-a-i-l-dotcom>
2018-09-11 16:13:03 +0200

    compiler efficiency baseline: naive big table

--- compiler/scripts/print_runtime_stats.py
+++ compiler/scripts/print_runtime_stats.py
@@ -12,6 +12,18 @@ tables = {}
 total_entries = 0
 total_miss_entries = 0
 states = set()
+terminal_states_cnt = 0
+
+trans = {}
+def addTrans(q1, q2):
+    if q1 not in trans:
+        trans[q1] = set()
+    trans[q1].add(q2)
+
+def countPaths(q=0):
+    if q not in trans: return 1
+    return sum(countPaths(q2) for q2 in trans[q])
+
 
 with (sys.stdin if filename == '-' else open(filename, 'r')) as fd:
     for line in fd:
@@ -32,11 +44,16 @@ with (sys.stdin if filename == '-' else open(filename, 'r')) as fd:
             is_range = True
             tbl_name = full_tbl_name[:-6]
             range_match = line.split()[4]
+        elif full_tbl_name == "query_actions":
+            terminal_states_cnt += 1
+            continue
         else:
             continue
 
-        state = int(line.split('=>')[1].split()[0])
-        states.add(state)
+        state = int(line.split()[3])
+        next_state = int(line.split('=>')[1].split()[0])
+        states.add(next_state)
+        addTrans(state, next_state)
 
         if tbl_name not in tables:
             tables[tbl_name] = dict(exact=0,
@@ -74,7 +91,9 @@ for tbl_name in tables:
     print
 
 
+print "paths:\t\t\t", countPaths(0)
 print "states:\t\t\t", len(states)
+print "terminal_states:\t\t\t", terminal_states_cnt
 print "tables:\t\t\t", len(tables)
 print "total_miss_entries:\t", total_miss_entries
 print "total_entries:\t\t", total_entries
