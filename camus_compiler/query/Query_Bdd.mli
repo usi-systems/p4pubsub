9658c418dc200cc08665b8893ce58ae51d12b200
Theo <theojepsen@g-m-a-i-l-dotcom>
2018-10-04 08:32:22 +0200

    moved compiler to new repo

--- camus_compiler/query/Query_Bdd.mli
+++
@@ -1,8 +0,0 @@
-open Core
-open Bdd
-open Query_Spec
-open Query_Types
-
-module QueryBdd : module type of Bdd(AtomicPredicate)(QueryAction)
-
-val bdd_from_rules : ?slices:int -> QuerySpec.t -> QueryRule.t list -> QueryBdd.t
