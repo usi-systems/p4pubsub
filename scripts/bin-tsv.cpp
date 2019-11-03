0be984a88a83d1f42f59ed96440077858c4f39f0
Theo <theojepsen@g-m-a-i-l-dotcom>
2019-11-03 12:28:47 +0100

    print percentiles and compile with O3

--- scripts/bin-tsv.cpp
+++ scripts/bin-tsv.cpp
@@ -12,12 +12,20 @@
 using std::cout;
 using std::endl;
 
+
+unsigned long percentile(std::vector<long> &v, double percent) {
+    auto nth = v.begin() + (percent*v.size())/100;
+    std::nth_element(v.begin(), nth, v.end());
+    return *nth;
+}
+
 long calc_avg(std::vector<long>::iterator begin, std::vector<long>::iterator end) {
   long sum = 0, cnt = 0;
   for (std::vector<long>::iterator it = begin; it != end; it++) {
     sum += *it;
     cnt++;
   }
+  if (cnt == 0) return 0;
   return sum / cnt;
 }
 
@@ -46,7 +54,20 @@ int main(int argc, char * argv[]) {
     while (is >> a) data.push_back(a);
   }
 
+  if (data.size() == 0) {
+    std::cerr << "No data." << std::endl;
+    return 0;
+  }
+
   std::sort(data.begin(), data.end());
+
+  std::vector<double> percentiles = {50.0, 90.0, 95.0, 99.0, 99.9, 99.99};
+  std::cerr << "Mean: " << calc_avg(data.begin(), data.end()) << std::endl;
+  std::cerr << "Percentiles: ";
+  for (auto p : percentiles) {
+    std::cerr << "  " << p << ": " << percentile(data, p) << ",";
+  }
+  std::cerr << std::endl;
 
   std::vector<long> xs;
   unsigned bin_size = data.size() / num_bins;
